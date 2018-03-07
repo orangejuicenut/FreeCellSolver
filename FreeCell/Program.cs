c97bc7037d0bf30cfa911a0c471763b8d46f8e23
Jacob Siegel <jacob.h.siegel@uscis.dhs.gov>
2018-03-07 09:25:00 -0500

    cleaned up some unused packages

--- FreeCell/Program.cs
+++ FreeCell/Program.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +10,7 @@ namespace FreeCell
     {
         static void Main(string[] args)
         {
-            string perfJson = System.IO.File.ReadAllText("..\\..\\PerfFun.json");
-            JArray perf = JArray.Parse(perfJson);
-
-            string testJson = System.IO.File.ReadAllText("..\\..\\Fun.json");
-            JArray test = JArray.Parse(testJson);
-
-            foreach(var obj in perf)
-            {
-
-                Console.WriteLine(test.Contains(obj));
-            }
-
-            //Console.WriteLine(perf.Contains(test[0]));
-            Console.WriteLine(perf[0]);
+            
 
         }
     }
